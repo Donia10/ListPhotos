0df443028eeea4f5ace3eedb5c106e9f84b35035
donia@programmingmap.com <donia@programmingmap.com>
2022-11-05 01:51:37 +0200

    setup project with mvvm-cleanArchitecture

---
+++ PhotosList/PhotosList/Presentation/Scene/PhotosList/View/PhotosListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosList
+//
+//  Created by ahmed sabry on 05/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
